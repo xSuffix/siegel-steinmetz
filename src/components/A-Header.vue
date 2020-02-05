fe8150ecc91fdd6418756044fd5d8fdfa74038b8
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2020-02-05 21:40:18 +0100

    Compress header image

--- src/components/A-Header.vue
+++ src/components/A-Header.vue
@@ -2,25 +2,25 @@
   <header>
     <nav>
       <router-link to="/">
-        <img src="../assets/favicon.svg" height="80%">
+        <img src="../assets/favicon.svg" height="80%" />
         <LogoClaim />
       </router-link>
       <div>
         <router-link to="/about">About</router-link>
-      </div>      
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
-import LogoClaim from '@/components/LogoClaim.vue'
+import LogoClaim from "@/components/LogoClaim.vue";
 
 export default {
-  name: 'A-Header',
+  name: "A-Header",
   components: {
     LogoClaim
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -29,7 +29,7 @@ header {
   background-size: cover;
   height: calc(128px + 30vw);
   max-height: 768px;
-  max-width: 1920px;  
+  max-width: 1920px;
   position: relative;
   margin: 0 auto;
 }
@@ -39,7 +39,7 @@ nav {
   position: absolute;
   height: calc(38.4px + 3vw);
   max-height: 96px;
-  background: rgb(228,229,221,0.75);
+  background: rgb(228, 229, 221, 0.75);
   bottom: 0;
   backdrop-filter: blur(6px);
   padding: 0 4%;
@@ -48,15 +48,14 @@ nav {
 
   > a {
     height: 100%;
+    min-width: 17.5%;
     box-sizing: content-box;
-    min-width: fit-content;
     display: inline-flex;
     align-items: center;
     text-decoration: none;
-    
+
     > img {
-      // width: 40%;
-      padding: 0 10%;
+      margin: 0 auto;
     }
   }
 }
